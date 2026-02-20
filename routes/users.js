2596d944e52384b864aa750f4f0df05b3cac312e
fares8lefi <fareslefi4@gmail.com>
2026-02-20 12:25:56 +0100

    add invite user function

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const userController= require('../Controllers/userController');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post('/invite', userController.inviteUser);
 module.exports = router;
