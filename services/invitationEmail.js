575aa278c93f2eb08899f7505b5fa18d0aa6bbab
fares8lefi <fareslefi4@gmail.com>
2026-02-20 12:23:06 +0100

    add email services

---
+++ services/invitationEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+module.exports.sendInvitationEmail = async (user, token) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: "Code d'activation - Scolarité App",
+      text: `Bonjour ${user.firstName},\n\nVous avez été invité à rejoindre la plateforme. Voici votre code d'activation : ${code}\n\nCe code est valable pendant 24 heures.`,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+  }
+};
