73901d8e68704de320c17867e4a67668d3028826
fares8lefi <fareslefi4@gmail.com>
2026-03-16 14:41:32 +0100

    add b=validation for parents children

--- validations/userValidation.js
+++ validations/userValidation.js
@@ -1,17 +1,24 @@
-import Joi from "joi";
+const Joi = require("joi");
 
-export const validateInvite = (user) => {
+exports.validateInvite = (user) => {
   const schema = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     email: Joi.string().email().required(),
     role: Joi.string().valid("ADMIN", "TEACHER", "PARENT").required(),
     phone: Joi.string(),
+    classId: Joi.string(),
+    children: Joi.array().items(
+      Joi.object({
+        name: Joi.string().required(),
+        classId: Joi.string().required()
+      })
+    )
   });
   return schema.validate(user);
 };
 
-export const validateLogin = (data) => {
+exports.validateLogin = (data) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
@@ -19,7 +26,7 @@ export const validateLogin = (data) => {
   return schema.validate(data);
 };
 
-export const validateAcceptInvitation = (data) => {
+exports.validateAcceptInvitation = (data) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
     invitationCode: Joi.string().required(),
