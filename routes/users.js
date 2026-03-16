8e1c5be51ce56ffa4104d5c6aa75c5d8a89307f0
fares8lefi <fareslefi4@gmail.com>
2026-03-16 14:37:40 +0100

    add crud router for user

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,9 @@ router.post('/invite', userController.inviteUser);
 router.post('/signup', userController.singup);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
+router.get('/getUsers', userController.getUsers);
+router.get('/getUserById/:id', userController.getUserById);
+router.put('/updateUser/:id', userController.updateUser);
+router.delete('/deleteUser/:id', userController.deleteUser);
 
 module.exports = router;
